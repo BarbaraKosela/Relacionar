e30184951688963d9502a27673bf9b57ea477dd4
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-06-29 17:12:36 -0300

    Organizar e funcionar;

--- ALUNOS.sql
+++ ALUNOS.sql
@@ -27,23 +27,33 @@
 
 	INSERT INTO caracteristicas(id_alunos, nome) VALUES 
 	((SELECT id FROM alunos WHERE nome = 'Heitor'), 'Egoísta'),
-	((SELECT id FROM alunos WHERE nome = 'Alice'), 'Organizando(a)'),
-	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Pontual'),
-	((SELECT id FROM alunos WHERE nome = 'Miguel'), 'Criativo(a)'),
-	((SELECT id FROM alunos WHERE nome = 'Heitor'), 'Proativo(a)'),
-	((SELECT id FROM alunos WHERE nome = 'Alice'), 'Altruista'),
-	((SELECT id FROM alunos WHERE nome = 'Valentina'), 'Pessimista'),
-	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Flexível'),
-	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Observadora'),
-	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Paciente'),
 	((SELECT id FROM alunos WHERE nome = 'Heitor'), 'Indelicado(a)'),
+	((SELECT id FROM alunos WHERE nome = 'Heitor'), 'Proativo(a)'),
+	((SELECT id FROM alunos WHERE nome = 'Heitor'), 'Egoísta');
+
+	INSERT INTO caracteristicas(id_alunos, nome) VALUES 
+	((SELECT id FROM alunos WHERE nome = 'Alice'), 'Organizando(a)'),
+	((SELECT id FROM alunos WHERE nome = 'Alice'), 'Altruista');
+
+	INSERT INTO caracteristicas(id_alunos, nome) VALUES 
+	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Pontual'),
+	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Observadora'),
 	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Desobediente'),
-	((SELECT id FROM alunos WHERE nome = 'Miguel'), 'Intolerante'),
+	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Sensível'),
+	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Egoísta');
+
+	INSERT INTO caracteristicas(id_alunos, nome) VALUES 
+	((SELECT id FROM alunos WHERE nome = 'Miguel'), 'Criativo(a)'),
+	((SELECT id FROM alunos WHERE nome = 'Miguel'), 'Intolerante');
+
+	INSERT INTO caracteristicas(id_alunos, nome) VALUES 
+	((SELECT id FROM alunos WHERE nome = 'Valentina'), 'Pessimista');
+
+	INSERT INTO caracteristicas(id_alunos, nome) VALUES 
+	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Flexível'),
+	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Paciente'),
 	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Empático(a)'),
-	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Egoísta'),
-	((SELECT id FROM alunos WHERE nome = 'Heitor'), 'Egoísta'),
-	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Altruísta'),
-	((SELECT id FROM alunos WHERE nome = 'Sophia'), 'Sensível');
+	((SELECT id FROM alunos WHERE nome = 'Joaquim'), 'Altruísta');
 
 	SELECT * FROM caracteristicas;
 	SELECT * FROM alunos;
